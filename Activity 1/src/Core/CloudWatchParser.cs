b61ac5a4fdb11379cd972284b5b6231bd558e808
Steve Gordon <sgordon@hotmail.co.uk>
2019-08-06 17:26:06 +0100

    initial

---
+++ Activity 1/src/Core/CloudWatchParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class CloudWatchParser
+    {
+        public CloudWatchParser()
+        {
+
+        }
+
+        public IReadOnlyCollection
+    }
+}
