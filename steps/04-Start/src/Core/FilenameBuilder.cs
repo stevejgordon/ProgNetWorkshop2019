3a4a2cd0aa21fbb263c1db9385b61e13fc1747cd
Steve <sgordon@hotmail.co.uk>
2019-08-15 16:00:04 +0100

    String create steps

---
+++ steps/04-Start/src/Core/FilenameBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Core
+{
+    public class FilenameBuilder
+    {
+        public const string DateFormat = "yyyy-MM-dd-HH-";
+        public const string Suffix = ".txt";
+        public static char Separator = '-';
+
+        public string BuildFilename(DataContext dataContext)
+        {
+            var date = dataContext.EventDateUtc.ToString(DateFormat, DateTimeFormatInfo.InvariantInfo);
+
+            return string.Concat(date, dataContext.Title.ToUpper(), Separator, dataContext.Key, Suffix);
+        }
+
+        public string BuildFilenameWithStringBuilder(DataContext dataContext)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(dataContext.EventDateUtc.ToString(DateFormat, DateTimeFormatInfo.InvariantInfo));
+            sb.Append(dataContext.Title.ToUpper()).Append('-');
+            sb.Append(dataContext.Key).Append('-');
+            sb.Append(Suffix);
+
+            return sb.ToString();
+        }
+    }
+
+    public class DataContext
+    {
+        public string Title { get; set; }
+        public string Key { get; set; }
+        public DateTime EventDateUtc { get; set; }
+    }
+}
---
+++ steps/04-Start/test/Core.Tests/FilenameBuilderTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+
+namespace Core.Tests
+{
+    public class FilenameBuilderTests
+    {
+        [Fact]
+        public void BuildFilename_ReturnsCorrectString()
+        {
+            var sut = new FilenameBuilder();
+
+            var result = sut.BuildFilename(TestContext);
+
+            Assert.Equal(Expected, result);
+        }
+
+        [Fact]
+        public void BuildFilenameWithStringBuilder_ReturnsCorrectString()
+        {
+            var sut = new FilenameBuilder();
+
+            var result = sut.BuildFilenameWithStringBuilder(TestContext);
+
+            Assert.Equal(Expected, result);
+        }
+
+        public DataContext TestContext => new DataContext
+        {
+            EventDateUtc = new DateTime(2019, 09,13, 10,00,00, DateTimeKind.Utc),
+            Title = "ProgNet2019",
+            Key = "WS"
+        };
+
+        public string Expected => "2019-09-13-10-PROGNET2019-WS.txt";
+    }
+}
